1dde875e16de578511a875a37a899aca1c0c2c10
apurba <apurba.nag@in.ey.com>
2024-05-08 23:05:07 +0530

    firebase deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:curiosify_ui_assignment/firebase_options.dart';
 import 'package:curiosify_ui_assignment/infrastructure/navigation/navigation.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
